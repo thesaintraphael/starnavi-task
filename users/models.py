3220eb50de70a20840a04e12070c23e2c1265e8a
Rafael Salimov <raphasalimov@gmail.com>
2022-08-13 14:56:45 +0400

    added fullname property to user model

--- users/models.py
+++ users/models.py
@@ -5,3 +5,7 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
 
     last_request_date = models.DateTimeField(null=True, blank=True)
+
+    @property
+    def fullname(self) -> str:
+        return self.get_full_name()
