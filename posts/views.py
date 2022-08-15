844e43aa5bd223bd4a3feb2fbf1c2ea4a83fe92b
Rafael Salimov <raphasalimov@gmail.com>
2022-08-15 08:06:07 +0400

    removed unused commit

--- posts/views.py
+++ posts/views.py
@@ -3,7 +3,7 @@ from django.db.models.functions import ExtractDay
 
 from rest_framework import generics
 from rest_framework.response import Response
-from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.permissions import IsAuthenticated
 from django_filters.rest_framework.backends import DjangoFilterBackend
 
 from .filters import LikeFilter
